44c349ffec62913f93195edc2e1910c982e64caf
Rodrigo sepulveda <150185578+rsepulvedaawto@users.noreply.github.com>
2025-06-10 14:32:04 -0400

    refactor: Remove UUID generation from the database and delegate it to the business logic layer.

--- src/main/java/com/nisum/challenge/adapter/out/persistence/entity/UserEntity.java
+++ src/main/java/com/nisum/challenge/adapter/out/persistence/entity/UserEntity.java
@@ -4,13 +4,10 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-import org.hibernate.annotations.UuidGenerator;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.OneToMany;
@@ -25,8 +22,6 @@ import lombok.Setter;
 public class UserEntity {
 
 	@Id
-	@GeneratedValue
-	@UuidGenerator
 	private UUID id;
 
 	private String name;
